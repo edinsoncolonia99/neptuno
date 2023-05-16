5101eea55ada76b233f6fdf8d081ee155e08c104
edinson <edinsoncolonia@gmail.com>
2023-05-16 15:59:32 -0500

    ventas2

--- src/app/reporventa/reporventa.component.ts
+++ src/app/reporventa/reporventa.component.ts
@@ -2,19 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-reporventa',
   templateUrl: './reporventa.component.html',
   styleUrls: ['./reporventa.component.css']
 })
-export class ReporventaComponent implements OnInit{
+export class ReporventaComponent implements OnInit {
 
-  ventaConIva: number | undefined;
-  totalUnidades: number | undefined;
-  totalFacturas: number | undefined;
   reporteForm: FormGroup;
-  
+
   constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar) {
     this.reporteForm = this.formBuilder.group({
       ventaConIva: ['', Validators.required],
@@ -23,23 +19,29 @@ export class ReporventaComponent implements OnInit{
     });
   }
 
-  
   ngOnInit(): void {}
-
-  //submitForm() {
-    // Lógica para procesar los datos enviados en el formulario
-  //console.log('Datos enviados:', this.ventaConIva, this.totalUnidades, this.totalFacturas);
-  // }
 
   cancelar() {
     // Lógica para cancelar el formulario o realizar alguna acción adicional
   }
 
   aceptar() {
-    console.log('Datos enviados:', this.ventaConIva, this.totalUnidades, this.totalFacturas);
     if (this.reporteForm.invalid) {
       this.snackBar.open('Por favor, complete todos los campos.', 'Cerrar', { duration: 3000 });
       return;
     }
+
+    const ventaConIva = this.reporteForm.get('ventaConIva')?.value;
+    const totalUnidades = this.reporteForm.get('totalUnidades')?.value;
+    const totalFacturas = this.reporteForm.get('totalFacturas')?.value;
+
+    if (confirm('¿Los datos son correctos?')) {
+      console.log('Datos enviados:', ventaConIva, totalUnidades, totalFacturas);
+      // subir a servidor
+      this.snackBar.open('Los datos han sido enviados', 'Cerrar', { duration: 4000 });
+      this.reporteForm.reset();
+
+  }
+  
 }
 }

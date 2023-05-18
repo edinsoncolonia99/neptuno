6baceff7c2ec98daa063b39b7eacc1b291a84610
edinson <edinsoncolonia@gmail.com>
2023-05-17 21:33:10 -0500

    mensaje

--- src/app/resulventa/resulventa.component.ts
+++ src/app/resulventa/resulventa.component.ts
@@ -9,20 +9,25 @@ import { BaseventaService } from '../baseventa.service';
 })
 export class ResulventaComponent implements OnInit {
   public baseventa: ObjetoModelo[] = [];
-  public ventapromedio: number = 0; // Variable para almacenar el cálculo ventapromedio
-  public unidadpromedio: number = 0; // Variable para almacenar el cálculo unidadpromedio
-  public preciopromedio: number = 0; // Variable para almacenar el cálculo facturapromedio
+  public ventapromedio: number = 0;
+  public unidadpromedio: number = 0;
+  public preciopromedio: number = 0;
+  public totalVentas: number = 0;
+  public totalunidades: number = 0;
+  public totalFacturas: number = 0;
+  public startDateInput: string = ''; // Variable para almacenar la fecha inicial seleccionada
+  public endDateInput: string = ''; // Variable para almacenar la fecha final seleccionada
 
   constructor(public baseventaService: BaseventaService) {}
 
   ngOnInit(): void {
     this.baseventa = this.baseventaService.getContacts();
     this.calcularIndicadores();
-    
   }
 
-  consultar() {
-    // Lógica para realizar la consulta adicional
+  consultar(): void {
+    console.log('Fecha inicial:', this.startDateInput);
+    console.log('Fecha final:', this.endDateInput);
   }
 
   calcularIndicadores() {
@@ -35,18 +40,16 @@ export class ResulventaComponent implements OnInit {
         totalVentas += venta.ventas;
         totalFacturas += venta.facturas;
         totalunidades += venta.unidades;
-  
       }
 
       if (totalFacturas !== 0) {
         this.ventapromedio = totalVentas / totalFacturas;
-        this.unidadpromedio = totalunidades / totalFacturas
-        this.preciopromedio = totalVentas / totalunidades
+        this.unidadpromedio = totalunidades / totalFacturas;
+        this.preciopromedio = totalVentas / totalunidades;
+        this.totalVentas = totalVentas;
+        this.totalunidades = totalunidades;
+        this.totalFacturas = totalFacturas;
       }
     }
   }
-
-  
-
 }
-

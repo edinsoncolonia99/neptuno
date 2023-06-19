85e6258e0182a92f8fd1474c28cd923fc2c35be1
edinson <edinsoncolonia@gmail.com>
2023-06-19 18:17:00 -0500

    Modularizar routing module se retira del app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MenuppalComponent } from '../app/home/menuppal/menuppal.component';
+import { LoginComponent} from '../app/login/login.component';
+import { IndicadorComponent } from '../app/admin/pages/indicador/indicador.component';
+import { ReporventaComponent} from '../app/admin/pages/reporventa/reporventa.component';
+import { ResumenComponent} from '../app/admin/pages/resumen/resumen.component';
+import { UsuariosComponent } from '../app/admin/pages/usuarios/usuarios.component';
+import { OpersubmenuComponent } from '../app/admin/pages/opersubmenu/opersubmenu.component';
+import { VentaoperativaComponent } from '../app/admin/pages/ventaoperativa/ventaoperativa.component';
+import { NotFoundComponent } from '../app/home/not-found/not-found.component';
 
+const appRoutes: Routes = [
+  {path: 'menuppal', component:MenuppalComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'indicador', component:IndicadorComponent},
+  {path: 'reporteventa', component:ReporventaComponent},
+  {path: 'resumen', component:ResumenComponent}, 
+  {path: 'usuarios', component:UsuariosComponent},
+  {path: 'opersubmenu', component:OpersubmenuComponent},
+  {path: 'resultado', component:VentaoperativaComponent}, //trasladar a ResulventaCom..
+  {path: '',
+  redirectTo: '/menuppal',
+  pathMatch: 'full'
+},
+  {path: '**' , component:NotFoundComponent}
+];
 
 
 
 @NgModule({
   //imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:[
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
+  ], 
+  exports: [
+    RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-
-import { AppRoutingModule } from './app-routing.module';
-import { RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
@@ -22,12 +16,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule} from '@angular/material/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
-
 import { NotFoundComponent } from './home/not-found/not-found.component';
 import { IndicadorComponent } from './admin/pages/indicador/indicador.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsuariosComponent} from './admin/pages/usuarios/usuarios.component';
-
 import { ResumenComponent} from './admin/pages/resumen/resumen.component';
 import { ReporventaComponent } from './admin/pages/reporventa/reporventa.component';
 import { MenuppalComponent } from './home/menuppal/menuppal.component';
@@ -37,25 +29,8 @@ import { LoginComponent } from './login/login.component';
 import { VentaoperativaComponent } from './admin/pages/ventaoperativa/ventaoperativa.component';
 import { ObjetoModelo } from './core/models/baseventa.model';
 import { UsuarioService } from './core/services/usuario.service';
+import { AppRoutingModule } from '../app/app-routing.module';
 
-
-
-const appRoutes: Routes = [
-  
-  {path: 'menuppal', component:MenuppalComponent},
-  {path: 'login', component:LoginComponent},
-  {path: 'indicador', component:IndicadorComponent},
-  {path: 'reporteventa', component:ReporventaComponent},
-  {path: 'resumen', component:ResumenComponent}, 
-  {path: 'usuarios', component:UsuariosComponent},
-  {path: 'opersubmenu', component:OpersubmenuComponent},
-  {path: 'resultado', component:VentaoperativaComponent}, //trasladar a ResulventaCom..
-  {path: '',
-  redirectTo: '/menuppal',
-  pathMatch: 'full'
-},
-  {path: '**' , component:NotFoundComponent}
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -81,7 +56,6 @@ const appRoutes: Routes = [
     MatListModule,
     MatCardModule,
     MatMenuModule,
-    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     MatDatepickerModule,
     MatFormFieldModule,
     MatInputModule,
@@ -89,8 +63,8 @@ const appRoutes: Routes = [
     MatSnackBarModule,
     ReactiveFormsModule,
     MatIconModule,
-    NgxChartsModule
-    
+    NgxChartsModule,
+    AppRoutingModule
   ],
   providers: [VolverService, UsuarioService, LoginComponent],
   bootstrap: [AppComponent],
